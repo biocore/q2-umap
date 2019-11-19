7884f775409bb42e9337ab8ea765d48176450c35
George Armstrong <garmstrong@colgate.edu>
2019-11-19 09:53:30 -0800

    MAINT: change metric to umap_metric

--- q2_umap/_method.py
+++ q2_umap/_method.py
@@ -12,7 +12,7 @@ _ADDITIONAL_METRICS = ['aitchison']
 _VALID_METRICS = _ADDITIONAL_METRICS + _SK_VALID_METRICS
 
 
-def distances(table: pd.DataFrame, metric: str = 'euclidean',
+def distances(table: pd.DataFrame, umap_metric: str = 'euclidean',
               n_components: int = 3, pseudocount: int = 1,
               umap_args: Union[str, dict] = None) -> skbio.DistanceMatrix:
 
@@ -22,11 +22,11 @@ def distances(table: pd.DataFrame, metric: str = 'euclidean',
     elif isinstance(umap_args, str):
         umap_args = literal_eval(umap_args)
 
-    if (metric not in _VALID_METRICS and not callable(metric) and metric is
+    if (umap_metric not in _VALID_METRICS and not callable(umap_metric) and umap_metric is
             not None):
         raise ValueError("Unknown metric %s. "
                          "Valid metrics are %s, or 'precomputed', or a "
-                         "callable" % (metric, _VALID_METRICS))
+                         "callable" % (umap_metric, _VALID_METRICS))
     counts = table.values
     if counts.sum() == 0:
         raise ValueError("The provided table object is empty")
@@ -34,12 +34,12 @@ def distances(table: pd.DataFrame, metric: str = 'euclidean',
     # define aitchison distance calculation
     def aitchison(x, y, **kwds):
         return euclidean(clr(x), clr(y))
-    if metric == 'aitchison':
+    if umap_metric == 'aitchison':
         counts += pseudocount
-        metric = aitchison
+        umap_metric = aitchison
 
     # run UMAP
-    reducer = umap.UMAP(n_components=n_components, metric=metric,
+    reducer = umap.UMAP(n_components=n_components, metric=umap_metric,
                         random_state=42, **umap_args)
     embedding = reducer.fit_transform(counts)
 
