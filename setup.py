a51af049b614774151bd7137aeffc3c038d0f013
George Armstrong <garmstrong@colgate.edu>
2019-11-18 16:30:00 -0800

    BLD: intial setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="q2-umap",
+    packages=find_packages(),
+    author="George Armstrong",
+    author_email="garmstro@eng.ucsd.edu",
+    description="Sample Embedding with UMAP",
+    license='BSD-3',
+    entry_points={
+        'qiime2.plugins': ['q2-umap=q2_umap.plugin_setup:plugin']
+    },
+)
