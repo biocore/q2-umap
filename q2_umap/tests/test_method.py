dfcf49757c0b1ddbcacaee7402162fc473be8622
George Armstrong <garmstrong@colgate.edu>
2019-11-21 09:58:49 -0800

    TST: additional tests for distances method

--- q2_umap/tests/test_method.py
+++ q2_umap/tests/test_method.py
@@ -26,6 +26,27 @@ class TestUMAPMethod(unittest.TestCase):
                        umap_args=umap_kwargs)
         self.assertEqual(dm.shape, (6, 6))
 
+    def test_umap_runner_no_args(self):
+        dm = distances(self.data)
+        self.assertEqual(dm.shape, (6, 6))
+
+    def test_umap_callable_metric(self):
+        def my_metric(x, y, **kwargs):
+            return sum(x - y)
+        umap_kwargs = {'n_neighbors': 3}
+        dm = distances(self.data, umap_metric=my_metric,
+                       umap_args=umap_kwargs)
+        self.assertEqual(dm.shape, (6, 6))
+
+    def test_umap_no_table_empty_error(self):
+        data = pd.DataFrame([[0, 0], [0, 0]])
+        with self.assertRaisesRegex(ValueError, r'is empty'):
+            distances(data)
+
+    def test_umap_unknown_metric_error(self):
+        with self.assertRaisesRegex(ValueError, r'Unknown metric'):
+            distances(self.data, umap_metric='unknown_metric')
+
 
 if __name__ == '__main__':
     unittest.main()
