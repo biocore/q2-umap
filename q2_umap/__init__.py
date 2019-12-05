fa9d58befc6cf977c8047e07f263c1fb5e4efd36
George Armstrong <garmstrong@colgate.edu>
2019-12-05 12:42:50 -0800

    DEV: add distances_phylogenetic to __init__.py

--- q2_umap/__init__.py
+++ q2_umap/__init__.py
@@ -1,3 +1,3 @@
-from q2_umap._method import pipeline, distances
+from q2_umap._method import pipeline, distances, distances_phylogenetic
 
-__all__ = ['pipeline', 'distances']
+__all__ = ['pipeline', 'distances', 'distances_phylogenetic']
