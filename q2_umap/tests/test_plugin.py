340a956afeccb9186bc206d2283d42127b5ef449
George Armstrong <garmstrong@colgate.edu>
2019-12-05 12:41:47 -0800

    TST: test for registered distances_phylogenetic

--- q2_umap/tests/test_plugin.py
+++ q2_umap/tests/test_plugin.py
@@ -1,12 +1,16 @@
+import io
+
+import skbio
 import pandas as pd
+import q2_umap.tests.base as test_base
+import pandas.testing as pdt
 from qiime2 import Metadata, Artifact
 from qiime2.plugin.testing import TestPluginBase
-import pandas.testing as pdt
 
 
 class TestUMAPPipeline(TestPluginBase):
 
-    package = 'q2_umap'
+    package = 'q2_umap.tests'
 
     def setUp(self):
         super().setUp()
@@ -74,7 +78,7 @@ class TestUMAPPipeline(TestPluginBase):
         self.assertEqual(repr(results.emperor.type), 'Visualization')
 
 
-class TestUMAPDistances(TestPluginBase):
+class TestUMAPDistancesPlugin(TestPluginBase):
     package = 'q2_umap'
 
     def setUp(self):
@@ -98,4 +102,36 @@ class TestUMAPDistances(TestPluginBase):
     def test_distances(self):
         table = Artifact.import_data('FeatureTable[Frequency]', self.data)
         results = self.distance(table, umap_args="{'n_neighbors': 3}")
+        self.assertEqual(repr(results.distance_matrix.type), 'DistanceMatrix')
+
+
+class TestUMAPDistancesPhylogeneticPlugin(TestPluginBase, test_base.TestCase):
+    package = 'q2_umap'
+
+    def setUp(self):
+        TestPluginBase.setUp(self)
+        test_base.TestCase.setUp(self)
+        self.data = pd.DataFrame([[1, 2, 0, 4],
+                                  [1, 4, 3, 5],
+                                  [0, 0, 1, 2],
+                                  [0, 1, 2, 1],
+                                  [1, 2, 2, 0],
+                                  [0, 1, 1, 0]],
+                                 index=['S1', 'S2', 'S3', 'S4', 'S5',
+                                        'S6'],
+                                 columns=['O1', 'O2', 'O3', 'O4'])
+        self.tree = skbio.TreeNode.read(io.StringIO(
+            '((O1:0.25, (O4:0.25, O2:0.50):0.1):0.25, O3:0.75)root;'))
+        self.metadata = Metadata(
+            pd.DataFrame({'foo': ['1', '2', '3', '4', '5', '6']},
+                         index=pd.Index(
+                             ['S1', 'S2', 'S3', 'S4', 'S5', 'S6'],
+                             name='id')))
+        self.distance = self.plugin.methods['distances_phylogenetic']
+
+    def test_distances(self):
+        table = Artifact.import_data('FeatureTable[Frequency]', self.data)
+        tree = Artifact.import_data('Taxonomy[Rooted]', self.tree)
+        results = self.distance(table, tree, 'unweighted_unifrac',
+                                umap_args="{'n_neighbors': 3}")
         self.assertEqual(repr(results.distance_matrix.type), 'DistanceMatrix')
